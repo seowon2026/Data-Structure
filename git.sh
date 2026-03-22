9c60462d4c6d09ddb3c2dd4a4015635cbc9d670e
Nochang0 <hy040504@kakao.com>
2026-03-22 16:56:59 +0900

    2Grade Data-Structure update (2026-03-22 16:56:59)

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "커밋 메시지: " msg
+read -p "(데이터구조) 커밋 메시지: " msg
 
 # 입력이 비었으면 기본 메시지 사용
 if [ -z "$msg" ]; then
-    msg="2Grade Data-Structure update"
+    msg="2Grade Data-Structure update ($(date '+%Y-%m-%d %H:%M:%S'))"
 fi
 
 git add .
